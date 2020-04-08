e0c6aa09d75de2e20d2b3de7ed24b69e2c4ddbae
Antonio de Castro Orbea <castro.orbea@gmail.com>
2020-04-08 22:11:58 +0200

    Se añade funcionalidad en nueva rama

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hola Mundo");
         System.out.println("Adios mundo");
+        System.out.println("Por ahora, no");
 
     }
 }
